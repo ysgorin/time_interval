db7870e7b0d06850adca5a7bec925467efe14f17
ysgorin <ysgorin@gmail.com>
2025-03-27 09:28:42 -0400

    added add and mul magic methods

--- time_interval.py
+++ time_interval.py
@@ -14,23 +14,31 @@ class TimeInterval:
             raise ValueError('Hours, minutes, and seconds must be positive integers.')
         
         # Normalize the time values
-        total_seconds = (hours * 3600) + (minutes * 60) + seconds
-        self.hours = total_seconds // 3600
+        self.total_seconds = (hours * 3600) + (minutes * 60) + seconds
+        self.hours = self.total_seconds // 3600
 
         if self.hours > 99:
             raise ValueError('Hours cannot exceed 99.')
 
-        self.minutes = (total_seconds % 3600) // 60
-        self.seconds = total_seconds % 60
+        self.minutes = (self.total_seconds % 3600) // 60
+        self.seconds = self.total_seconds % 60
 
     def __str__(self):
         return f'{self.hours:02d}:{self.minutes:02d}:{self.seconds:02d}'
     
-    def __add__():
-        pass
+    def __add__(self, other):
+        result_total_seconds = self.total_seconds + other.total_seconds
+        result_hours = result_total_seconds // 3600
+        result_minutes = (result_total_seconds % 3600) // 60
+        result_seconds = result_total_seconds % 60
+        return f'{result_hours:02d}:{result_minutes:02d}:{result_seconds:02d}'
 
-    def __mul__():
-        pass
+    def __mul__(self, other):
+        result_total_seconds = self.total_seconds * other
+        result_hours = result_total_seconds // 3600
+        result_minutes = (result_total_seconds % 3600) // 60
+        result_seconds = result_total_seconds % 60
+        return f'{result_hours:02d}:{result_minutes:02d}:{result_seconds:02d}'
 
 tt1 = TimeInterval(5, 66, 80)
 print(tt1)
